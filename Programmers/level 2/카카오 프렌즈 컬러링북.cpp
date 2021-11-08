8592862644fe0147c4181a16d04b0775a2cc1484
Donghyun <kjyoa09@naver.com>
2021-11-08 21:11:09 +0900

    카카오 프렌즈 컬러링북

---
+++ Programmers/level 2/카카오 프렌즈 컬러링북.cpp
@@ -0,0 +1,78 @@
+#include <vector>
+#include <map>
+#include <iostream>
+#include <deque>
+
+/*
+1110
+1220
+1001
+0001
+0003
+0003
+*/
+
+/*
+1110
+1110
+0001
+0001
+0001
+0001
+*/
+using namespace std;
+
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+
+vector<vector<bool>> maps;
+vector<int> answer;
+
+void bfs(int m, int n,int r, int c, vector<vector<int>> &picture){
+    int cnt = 0, stan = picture[r][c];
+    deque<vector<int>> que;
+    int dx[4] = {1,0,-1,0},dy[4] = {0,1,0,-1};   
+    que.push_back({r,c});
+    maps[r][c] = true;
+    
+    while (!que.empty()){
+        vector<int> coord = que.front();
+        que.pop_front();
+        cnt ++;
+        for (int idx = 0; idx < 4; idx ++ ){
+            int px = coord[0] + dx[idx], py = coord[1] + dy[idx];
+            if (!(0<= px && px < m) || !(0<= py && py < n)){continue;}
+            if (picture[px][py] == stan && maps[px][py] == false){
+                maps[px][py] = true;
+                que.push_back({px,py});
+            }
+        }
+    }
+    answer[1] = answer[1] > cnt ? answer[1] : cnt;
+
+}
+
+vector<int> solution(int m, int n, vector<vector<int>> picture) {
+    maps.clear();
+    answer.clear();
+    
+    answer = {0,0};
+    for (int r = 0; r<m ; r++){
+        vector<bool> tmp;
+        for (int c=0; c<n; c++){
+            tmp.push_back(false);
+        }
+        maps.push_back(tmp);
+    }
+            
+                
+    for (int r = 0; r<m ; r++){
+        for (int c=0; c<n; c++){
+            if (picture[r][c] != 0 && maps[r][c] != 1){
+                answer[0]++;
+                bfs(m,n,r,c,picture);
+            } 
+        }
+    }    
+    
+    return answer;
+}

3807cd45630fe4c22c5ffae73333a00c089d3f59
Donghyun <kjyoa09@naver.com>
2021-11-09 17:04:11 +0900

    구명보트

---
+++ Programmers/level 2/구명보트.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0,lt = 0, rt = people.size()-1;
+    sort(people.begin(),people.end(),greater<int>());
+    while (lt<=rt){
+        answer++;
+        if (rt==lt){break;}
+        else if(people[lt] + people[rt] <= limit){lt++;rt--;}
+        else {lt++;}
+    }
+    return answer;
+}
